ac7ca83a8e8942faab597070125c76fd359a825c
nikitachairunnisa <85438402+nikitachairunnisa@users.noreply.github.com>
2023-08-05 23:45:33 +0700

    Update apk_prediksi.py

--- apk_prediksi.py
+++ apk_prediksi.py
@@ -2,6 +2,7 @@ import pickle
 import streamlit as st
 import pandas as pd
 import numpy as np
+import base64
 from sklearn.linear_model import Ridge
 from sklearn.preprocessing import LabelEncoder
 
